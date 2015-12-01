35ca6a7db99631d6630aed2ee81752f649f85cd2
Jorge Epuñan <juanbrujo@gmail.com>
2015-12-01 12:51:20 -0300

    first stable release

---
+++ README.md
@@ -0,0 +1,24 @@
+# random CLI
+
+*NodeJS CLI to select randomly from given list or range*
+
+## Commands:
+
+```
+-r, --range <int>..<int> // outputs random integer between 2 integers
+```
+-l, --list <element1>,<element2>,<elementN> // outputs random from list of elements
+-p, --password <length (int)> // outputs a random string from a map of characters
+```
+
+## Use:
+
+## Installation:
+
+Clone this repo and use it locally:
+
+```
+$ git clone 
+$ node bin/random-cli.js <option>
+```
+
---
+++ bin/randon-cli.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+/*!
+
+*/
+
+// var argv  = require('yargs')
+//   .option('layer', { alias: 'l', type: 'boolean' })
+//   .option('text', { alias: 't', type: 'boolean' })
+//   .argv,
+
+var program = require('commander');
+var crypto 	= require("crypto");
+var path    = require('path');
+var pkg     = require(path.join(__dirname, '../package.json'));
+ 
+function range(val) {
+  var firstNum = parseInt(val.split('..')[0]);
+  var lastNum = parseInt(val.split('..')[1]);
+  
+  return Math.floor( Math.random() * ( lastNum - firstNum + 1 ) + firstNum ); 
+}
+ 
+function list(val) {
+  var newList = [];
+  var values = val.split(',');
+
+  values.forEach(function(l){
+  	newList.push(l);
+  });
+
+  return newList[Math.floor(Math.random() * newList.length)]
+}
+
+function password(val) {
+	var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP1234567890";
+  var result = "";
+  for (var x = 0; x < val; x++) {
+      var i = Math.floor(Math.random() * chars.length);
+      result += chars.charAt(i);
+  }
+
+  return result;
+}
+ 
+program
+  .version(pkg.version)
+  .usage('[options] <file ...>')
+  .option('-r, --range <a>..<b>', 'A range', range)
+  .option('-l, --list <items>', 'A list', list)
+  .option('-p, --password <length>', 'Length', password)
+  .parse(process.argv);
+
+if(program.range) {
+	console.log(' ✔  Random from range: %j', program.range);
+}
+
+if(program.list) {
+	console.log(' ✔  Random from list: %j', program.list);
+}
+
+if(program.password) {
+	console.log(' ✔  Random password: %j', program.password);
+}
+
+
